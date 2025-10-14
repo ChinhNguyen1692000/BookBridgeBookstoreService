5a9062dc25c9956c520659a679e5238b1ad5024c
nlhchinh <chinhnguyen1692000z@gmail.com>
2025-10-14 14:50:56 +0700

    fix jwt, configuration

--- src/BookstoreService/BookstoreService.Api/Program.cs
+++ src/BookstoreService/BookstoreService.Api/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.EntityFrameworkCore;
 using AutoMapper;
 
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
 
 // Add services to the container.
 builder.Services.AddControllers();
@@ -92,7 +93,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
--- src/BookstoreService/BookstoreService.Api/obj/Debug/net8.0/BookstoreService.Api.AssemblyInfo.cs
+++ src/BookstoreService/BookstoreService.Api/obj/Debug/net8.0/BookstoreService.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookstoreService.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d40aa927d9991148ae63da1c768ef4d7a404ba6b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1d6e474c97c1c60cf91e6e9c2d51028da937b2c")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookstoreService.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookstoreService.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
